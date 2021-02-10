0563d383d3f69ab94c133db4a5d18d5b1e0ccf1b
ey-charles <carlos.castillo@hey.com>
2021-02-10 10:27:04 -0500

    Add my_each_with_index method

--- script.rb
+++ script.rb
@@ -7,8 +7,19 @@ module Enumerable
     end
   end
 
+  def my_each_with_index
+    i = 0
+
+    for value in self do
+      yield(value, i)
+      i += 1
+    end
+  end
+
 end
 
 p [1, 2, 3].my_each { |n| puts n }
-
 p [1, 2, 3].each { |n| puts n }
+p "------"
+p [1, 2, 3].my_each_with_index { |n, i| puts "index: #{i} for #{n}"}
+
