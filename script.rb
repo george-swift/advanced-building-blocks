f359bd8abd2e704a8de1de34eb230d553baf9775
George <ubong_george@ymail.com>
2021-02-10 18:44:06 +0100

    Add any? to enumerable

--- script.rb
+++ script.rb
@@ -32,6 +32,29 @@ module Enumerable
 
     return new_arr.size == self.size ? true : false
   end
+
+  def my_any?(pattern = nil, &block)
+    if block_given?
+      new_arr = self.my_select(&block)
+      return new_arr.size > 0 ? true : false
+    end
+    
+    if !pattern.nil?
+      for value in self do
+        if value.is_a? String 
+          return true if pattern.match? value
+        end
+
+        if pattern.is_a? Class
+          return true if value.is_a? pattern
+        else 
+          return true if value === pattern
+        end
+      end
+    end
+    
+    return false
+  end
 end
 
 # p [1, 2, 3].my_each { |n| puts n }
@@ -39,5 +62,7 @@ end
 # p [1, 2, 3].my_each_with_index { |n, i| puts "index: #{i} for #{n}"}
 # p "------"
 # p [1, 2, 3, 4, 5].my_select { |n| n.odd? }
-p "------"
-p [1, 3, 5].my_all? { |n| n.odd? }
+# p "------"
+# p [1, 3, 5].my_all? { |n| n.odd? }
+# p "------"
+p [1, 3, 'abcd'].my_any?(/d/)
