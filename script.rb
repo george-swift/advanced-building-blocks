06a7ca722cad07ecf31fe7e4c08dde01af0c82a8
George <ubong_george@ymail.com>
2021-02-10 16:43:42 +0100

    Add my_select method

--- script.rb
+++ script.rb
@@ -16,10 +16,26 @@ module Enumerable
     end
   end
 
+  def my_select
+    arr = Array.new
+    for value in self do
+      if yield(value)
+        arr << value
+      end
+    end
+    arr
+  end
+
 end
 
 p [1, 2, 3].my_each { |n| puts n }
 p [1, 2, 3].each { |n| puts n }
 p "------"
 p [1, 2, 3].my_each_with_index { |n, i| puts "index: #{i} for #{n}"}
+p "------"
+p [1, 2, 3, 4, 5].my_select { |n| n.odd? }
+
+
+
+
 
