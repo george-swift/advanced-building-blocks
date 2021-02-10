c44b54296f77b6779d3ae6ab75572435e0fe76c8
ey-charles <carlos.castillo@hey.com>
2021-02-10 11:00:16 -0500

    Add my_all? method

--- script.rb
+++ script.rb
@@ -18,6 +18,7 @@ module Enumerable
 
   def my_select
     arr = Array.new
+
     for value in self do
       if yield(value)
         arr << value
@@ -26,16 +27,17 @@ module Enumerable
     arr
   end
 
+  def my_all?(&block)
+    new_arr = self.my_select(&block)
+
+    return new_arr.size == self.size ? true : false
+  end
 end
 
-p [1, 2, 3].my_each { |n| puts n }
-p [1, 2, 3].each { |n| puts n }
+# p [1, 2, 3].my_each { |n| puts n }
+# p "------"
+# p [1, 2, 3].my_each_with_index { |n, i| puts "index: #{i} for #{n}"}
+# p "------"
+# p [1, 2, 3, 4, 5].my_select { |n| n.odd? }
 p "------"
-p [1, 2, 3].my_each_with_index { |n, i| puts "index: #{i} for #{n}"}
-p "------"
-p [1, 2, 3, 4, 5].my_select { |n| n.odd? }
-
-
-
-
-
+p [1, 3, 5].my_all? { |n| n.odd? }
